2d61608de6de12bfe13a43dbbf42dee3a43295ad
prady346 <prady346@gmail.com>
2025-12-18 22:21:41 +0530

    Setup Git LFS for video files and use local tutorial video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -211,7 +211,7 @@ const Home = () => {
                 style={{ width: '100%', height: '180px', borderRadius: '12px', objectFit: 'cover' }}
                 poster="/assets/playslogo.png"
               >
-                <source src="https://drive.google.com/uc?export=download&id=1ExdpITjQd-AumQhbghJqJwXrxcHbTjOV" type="video/mp4" />
+                <source src="/assets/tutorial.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
               <div style={{
